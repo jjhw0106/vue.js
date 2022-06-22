b63090a8aaf3e8fb9414d243fb09ec17e7e13684
DESKTOP-QUPDGLB\jjhw0 <jjhw0106@naver.com>
2022-06-22 17:14:24 +0900

    UPDATE : VUEJS 공부내용 업데이트 (INSTANCE, COMPONENT)

---
+++ app.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el: '#app',
+    data:{
+        message: 'this is a parent component'
+    }
+});
+
+var todoList = {
+    template: '<div>오늘 할 일</div>'
+}
---
+++ component/app.js
@@ -0,0 +1,3 @@
+var cmp ={
+    template: '<div>지역ffff 컴포넌트가 등록되었습니다</div>'
+}
