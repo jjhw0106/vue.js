9aa4e362f609d46f8885ef508a4a48888432489b
DESKTOP-QUPDGLB\jjhw0 <jjhw0106@naver.com>
2022-06-29 18:20:40 +0900

    뷰 todolist

---
+++ vue_todo/src/components/TodoHeader.vue
@@ -0,0 +1,21 @@
+<template>
+    <header>
+        <h1>TODO it!!</h1>
+    </header>
+</template>
+
+<script>
+export default { 
+
+
+}
+</script>
+
+<!--scpoed : 뷰에서 지원하는 속성, 스타일 정의를 해당 컴포넌트에만 적용하겠다는 의미 -->
+<style scoped> 
+   h1 {
+    color: #2f3b52;
+    font-weight: 900;
+    margin: 2.5rem 0 1.5rem;
+   }
+</style>
---
+++ vue_todo/src/components/TodoList.vue
@@ -0,0 +1,67 @@
+<template>
+    <section>
+        <ul>
+            <!-- index는 v-for디렉티브가 기본적으로 제공하는 변수 -->
+            <li v-for="(todoItem, index) in todoItems" :key="todoItem" class="shadow">
+                <i class="checkBtn fas fa-check" aria-hidden="true">
+                    {{ todoItem }}
+                </i>
+                <span class="removeBtn" type="button" @click="removeTodo(todoItem, index)">
+                    <i class="far fa-trash-alt" aria-hidden="true"></i>
+                </span>
+            </li>
+        </ul>
+    </section>
+</template>
+
+<script>
+export default { 
+    data(){
+        return {
+            todoItems: []
+        }
+    },
+    methods: {
+        removeTodo(todoItem, index){
+            localStorage.removeItem(todoItem);
+            this.todoItems.splice(index, 1);
+        }
+    },
+    created(){
+        if(localStorage.length>0){
+            for(var i =0; i < localStorage.length; i++){
+                this.todoItems.push(localStorage.key(i));
+            }
+        }
+    }, 
+}
+
+</script>
+
+<style scoped>
+    ul{
+        list-style-type: none;
+        padding-left: 0px;
+        margin-top: 0;
+        text-align: left;
+    }
+    li{
+        display: flex;
+        min-height: 50px;
+        height: 50px;
+        line-height: 50px;
+        margin: 0.5rem 0;
+        padding: 0 0.9rem;
+        background: white;
+        border-radius: 5px;
+    }
+    .checkBtn{
+        line-height: 45px;
+        color: #62acde;
+        margin-right: 5px;
+    }
+    .removeBtn{
+        margin-left: auto;
+        color:#de4343;
+    }
+</style>
